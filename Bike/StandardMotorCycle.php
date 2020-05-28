49559f334e9f62ee62ff5b80a08e0063e6d071ad
cecep29 <cecepefendi.c2p@gmail.com>
2020-05-28 22:35:40 +0700

    add SportBike and StandarMotor

---
+++ Bike/StandardMotorCycle.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Bike;
+
+use Bike;
+use Engine\MotorCycleEngine;
+
+class StandardMotorCycle extends Bike 
+{
+    private $motor_cycle_engine;
+    private $str = __METHOD__.' '.__CLASS__;
+
+    public function __construct(MotorCycleEngine $motor_cycle_engine)
+    {
+        $this->motor_cycle_engine = $motor_cycle_engine;
+    }
+
+    public function ride()
+    {
+        return $this->str;
+    }
+
+    public function run()
+    {
+        return $this->str;
+    }
+}
